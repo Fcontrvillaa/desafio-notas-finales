56a3498b353e770ec3d086d014a2a26aba697b44
Fcontrvillaa <fcontreras+1@gmail.com>
2025-04-24 00:44:24 +0000

    captura notas

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,35 @@
 
 alert ("mensaje") */
 
+
+
+    let nota1 = prompt('ingrese  la nota 1 de HTML:')
+    document.getElementById('nota1html').textContent = nota1
+ 
+    let nota2 = prompt('ingrese  la nota 2 de HTML:')
+    document.getElementById('nota2html').textContent = nota2
+
+    let nota3 = prompt('ingrese  la nota 3 de HTML:')
+    document.getElementById('nota3html').textContent = nota3
+
+
+
+    let css1 = prompt('ingrese  la nota 1 de CSS:')
+    document.getElementById('nota1css').textContent = css1
+ 
+    let css2 = prompt('ingrese  la nota 2 de CSS:')
+    document.getElementById('nota2css').textContent = css2
+
+    let css3 = prompt('ingrese  la nota 3 de CSS:')
+    document.getElementById('nota3css').textContent = css3
+
+
+
+    let js1 = prompt('ingrese  la nota 1 de JS:')
+    document.getElementById('nota1js').textContent = js1
+ 
+    let js2 = prompt('ingrese  la nota 2 de JS:')
+    document.getElementById('nota2js').textContent = js2
+
+    let js3 = prompt('ingrese  la nota 3 de JS:')
+    document.getElementById('nota3js').textContent = js3
